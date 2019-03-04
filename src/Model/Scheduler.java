26800a32a2afd7c2c6a2813e7392e670c57fff9e
Tomer <gross.tomer10@gmail.com>
2019-03-04 23:20:42 +0200

    add class - schedular

---
+++ src/Model/Scheduler.java
@@ -0,0 +1,21 @@
+package Model;
+
+import Model.User;
+
+public class Scheduler {
+
+    public Meeting setMeeting(User[] users){
+        return null;
+    }
+
+    public Meeting[] viewMeetings(User user) {
+        return null;
+    }
+
+    public void cancelMeeting(Meeting meeting) {}
+
+    public Meeting[] getMeetingsToday(){
+        return null;
+    }
+
+}
